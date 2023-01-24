93f9b2c31744a41866b393dae58a90689a060f9e
Ziloliddin <Zilolidddinkamolov@gmail.com>
2023-01-24 20:57:42 +0500

    Initial Commit

---
+++ Lottie_last/Lottie_lastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottie_lastApp.swift
+//  Lottie_last
+//
+//  Created by Ziloliddin Kamolov on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Lottie_lastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
